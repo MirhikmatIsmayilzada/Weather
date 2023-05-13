c31fc3122821f3bf29ee4237db286711160bc070
MirhikmatIsmayilzada <mirhikmatismayilzada@gmail.com>
2023-05-13 22:26:37 +0000

    end

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ $(document).ready(function () {
 
 $(document).on('keypress', function (e) {
     if (e.which == 13) {
-        let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${$("#myWeather").val()}& appid=b5599ba12340d62654be5c6cfb55ebd6 & units=metric`
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=b5599ba12340d62654be5c6cfb55ebd6&units=metric`
         async function myweather() {
             let responce = await fetch(url)
             let data = await responce.json()
-            console.log(data);
+            $("#city").html(data.name)
+            $("#temp").html(data.main.temp)
+            $("#feels").html(data.main.feels_like)
+            $("#myWeather").val("")
+            console.log(data)
         }
         myweather()
     }
@@ -20,14 +24,14 @@ $(document).on('keypress', function (e) {
 
 $("#btn").click(function (e) {
     e.preventDefault()
-    let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${$("#myWeather").val()}&appid=b5599ba12340d62654be5c6cfb55ebd6&units=metric`
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=b5599ba12340d62654be5c6cfb55ebd6&units=metric`
     async function myweather() {
         let responce = await fetch(url)
         let data = await responce.json()
         $("#city").html(data.name)
         $("#temp").html(data.main.temp)
         $("#feels").html(data.main.feels_like)
-        $("myWeather").val("")
+        $("#myWeather").val("")
         console.log(data)
     }
     myweather()
