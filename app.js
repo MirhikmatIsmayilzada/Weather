7b734e7cc7d10a19a2da7d3f8ad69ceff852fc87
MirhikmatIsmayilzada <mirhikmatismayilzada@gmail.com>
2023-05-27 05:52:31 +0000

    end

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 "use strict";
 $(document).ready(function () {
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=b5599ba12340d62654be5c6cfb55ebd6&units=metric`
+            async function myweather() {
+                let responce = await fetch(url)
+                let data = await responce.json()
+                $("#city").html(data.name)
+                $("#temp").html(data.main.temp)
+                $("#feels").html(data.main.feels_like)
+                $("#myWeather").val("")
+                console.log(data)
+            }
+            myweather()
+        }
+    })
 
-});
-
-
-
-
-$(document).on('keypress', function (e) {
-    if (e.which == 13) {
+    $("#btn").click(function (e) {
+        e.preventDefault()
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=b5599ba12340d62654be5c6cfb55ebd6&units=metric`
         async function myweather() {
             let responce = await fetch(url)
@@ -19,20 +29,9 @@ $(document).on('keypress', function (e) {
             console.log(data)
         }
         myweather()
-    }
-})
+    })
+});
 
-$("#btn").click(function (e) {
-    e.preventDefault()
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=b5599ba12340d62654be5c6cfb55ebd6&units=metric`
-    async function myweather() {
-        let responce = await fetch(url)
-        let data = await responce.json()
-        $("#city").html(data.name)
-        $("#temp").html(data.main.temp)
-        $("#feels").html(data.main.feels_like)
-        $("#myWeather").val("")
-        console.log(data)
-    }
-    myweather()
-})
+
+
+
